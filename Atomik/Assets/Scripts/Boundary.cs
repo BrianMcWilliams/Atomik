7c4556155b317470059f22b8e3aecd2e45235ff5
dvellucci <vellucci.dan@gmail.com>
2019-12-21 14:58:51 -0500

    Added a boundary to the map so that particles that go too far will be destroyed.

---
+++ Atomik/Assets/Scripts/Boundary.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundary : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //destroy the particle when it exits the boundary
+        Particle particle = other.gameObject.GetComponent<Particle>();
+        if(particle)
+        {
+            //since neutrons are not in the list, no need to call Remove()
+            if(particle.m_Charge != Charge.Neutral)
+                ParticleManager.GetChargedParticleList().Remove(particle);
+            Destroy(particle.gameObject);
+        }
+    }
+}
