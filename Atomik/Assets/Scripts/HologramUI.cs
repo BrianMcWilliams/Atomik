f9444df5cdb104c21e166c1b660a83d007c7efea
dvellucci <vellucci.dan@gmail.com>
2020-01-04 14:50:58 -0500

    Added basis for hologram ui.

---
+++ Atomik/Assets/Scripts/HologramUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HologramUI : MonoBehaviour
+{
+    public Transform m_Camera;
+
+    bool m_Hidden;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Hidden = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+}
