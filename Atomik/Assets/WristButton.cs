37b027a6b8b04dcddf224d383b0f4a1b933f3f01
Brian <brianmcwilliams15@gmail.com>
2020-01-08 20:57:43 -0500

    Adding wrist button to activate hologram UI

---
+++ Atomik/Assets/WristButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WristButton : MonoBehaviour
+{
+    AudioSource m_AudioSource;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "WristButton")
+        {
+            m_AudioSource.Play();   
+        }
+    }
+}
